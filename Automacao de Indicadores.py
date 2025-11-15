1c14dc46e8bd2a094847189451475fc7f48711b9
AntonnySC7 <antonny.sc@outlook.com>
2025-11-15 16:25:51 -0300

    Add files via upload

---
+++ Automacao de Indicadores.py
@@ -0,0 +1,263 @@
+# %% [markdown]
+# ### Passo 1 - Importar Arquivos e Bibliotecas
+
+# %%
+# Importar bibliotecas
+import pandas as pd
+import win32com.client as win32
+import pathlib
+
+# %%
+# Importar bases de dados
+
+emails = pd.read_excel(r'Bases de Dados\Emails.xlsx')
+lojas = pd.read_csv(r'Bases de Dados\Lojas.csv', encoding="latin1", sep=";")
+vendas = pd.read_excel(r'Bases de Dados\Vendas.xlsx')
+
+# %% [markdown]
+# ### Passo 2 - Definir Criar uma Tabela para cada Loja e Definir o dia do Indicador
+
+# %%
+# Incluir nome da Loja em vendas
+vendas = vendas.merge(lojas, on="ID Loja")
+
+# %%
+dicionario_lojas = {}
+
+for loja in lojas['Loja']:
+    dicionario_lojas[loja] = vendas.loc[vendas["Loja"] == loja, :]
+
+# %%
+# Pegando a data mais recente
+dia_indicador = vendas['Data'].max()
+print(dia_indicador)
+print(f'{dia_indicador.day}/{dia_indicador.month}')
+
+# %% [markdown]
+# ### Passo 3 - Salvar a planilha na pasta de backup
+
+# %%
+# Identificar se a pasta de backup
+caminho_backup = pathlib.Path(r'Backup Arquivos Lojas')
+
+arquivos_pasta_backup = caminho_backup.iterdir()
+
+lista_nomes_backup = [arquivo.name for arquivo in arquivos_pasta_backup]
+print(lista_nomes_backup)
+
+for loja in dicionario_lojas:
+    if loja not in lista_nomes_backup:
+        nova_pasta = caminho_backup / loja
+        nova_pasta.mkdir()
+
+    # Salvar dentro da pasta
+    nome_arquivo = f'{dia_indicador.month}_{dia_indicador.day}_{loja}.xlsx'
+    local_arquivo = caminho_backup / loja / nome_arquivo
+    dicionario_lojas[loja].to_excel(local_arquivo)
+
+# %% [markdown]
+# ### Passo 4 - Calcular o indicador para 1 loja
+
+# %%
+# Definicao de metas
+
+meta_faturamento_dia = 1000
+meta_faturamento_ano = 1650000
+
+meta_qtde_produtos_dia = 4
+meta_qtde_produtos_ano = 120
+
+meta_ticket_medio_dia = 500
+meta_ticket_medio_ano = 500
+
+# %%
+for loja in dicionario_lojas:
+    vendas_loja = dicionario_lojas[loja]
+    vendas_loja_dia = vendas_loja.loc[vendas_loja['Data'] == dia_indicador, :]
+
+    # Faturamento
+    faturamento_ano = vendas_loja['Valor Final'].sum()
+    faturamento_dia = vendas_loja_dia['Valor Final'].sum()
+
+    # Diversidade de Produtos
+    qtde_produtos_ano = len(vendas_loja['Produto'].unique())
+    qtde_produtos_dia = len(vendas_loja_dia['Produto'].unique())
+
+    # Ticket médio
+    valor_venda = vendas_loja.groupby('Código Venda').sum(numeric_only=True)
+    ticket_medio_ano = valor_venda['Valor Final'].mean()
+
+    valor_venda_dia = vendas_loja_dia.groupby('Código Venda').sum(numeric_only=True)
+    ticket_medio_dia = valor_venda_dia['Valor Final'].mean()
+
+    # Enviar E-mail
+    outlook = win32.Dispatch('outlook.application')
+
+    nome = emails.loc[emails['Loja']==loja, 'Gerente'].values[0]
+    email = outlook.CreateItem(0)
+
+    # Processo de envio de email
+    email.To = emails.loc[emails['Loja']==loja, 'E-mail'].values[0]
+    email.Cc = "programacao.oficial2@gmail.com"
+    email.Subject = f"OnePage Dia {dia_indicador.day}/{dia_indicador.month} - Loja {loja}"
+    # email.Body = "Texto do email"
+
+    if faturamento_dia >= meta_faturamento_dia:
+        cor_fat_dia = 'green'
+    else:
+        cor_fat_dia = 'red'
+    if faturamento_ano >= meta_faturamento_ano:
+        cor_fat_ano = 'green'
+    else:
+        cor_fat_ano = 'red'
+    if qtde_produtos_dia >= meta_qtde_produtos_dia:
+        cor_qtde_dia = 'green'
+    else:
+        cor_qtde_dia = 'red'
+    if qtde_produtos_ano >= meta_qtde_produtos_ano:
+        cor_qtde_ano = 'green'
+    else:
+        cor_qtde_ano = 'red'
+    if ticket_medio_dia >= meta_ticket_medio_dia:
+        cor_ticket_dia = 'green'
+    else:
+        cor_ticket_dia = 'red'
+    if ticket_medio_ano >= meta_ticket_medio_ano:
+        cor_ticket_ano = 'green'
+    else:
+        cor_ticket_ano = 'red'
+
+    # Enviando email com texto em HTML e anexando arquivo
+    email.HTMLBody = f"""<p>Bom dia, {nome}</p>
+
+    <p>O resultado de ontem <strong>({dia_indicador.day}/{dia_indicador.month})</strong> da <strong>Loja {loja}</strong> foi: </p>
+
+    <table>
+    <tr>
+        <th>Indicador</th>
+        <th style="text-align: center">Valor Dia</th>
+        <th style="text-align: center">Meta Dia</th>
+        <th style="text-align: center">Cenário Dia</th>
+    </tr>
+    <tr>
+        <td>Faturamento</td>
+        <td style="text-align: center">R${faturamento_dia:.2f}</td>
+        <td style="text-align: center">R${meta_faturamento_dia:.2f}</td>
+        <td style="text-align: center"><font color={cor_fat_dia}>◙</font></td>
+    </tr>
+    <tr>
+        <td>Diversidade de Produtos</td>
+        <td style="text-align: center">{qtde_produtos_dia}</td>
+        <td style="text-align: center">{meta_qtde_produtos_dia}</td>
+        <td style="text-align: center"><font color={cor_qtde_dia}>◙</font></td>
+    </tr>
+    <tr>
+        <td>Ticket Médio</td>
+        <td style="text-align: center">R${ticket_medio_dia:.2f}</td>
+        <td style="text-align: center">R${meta_ticket_medio_dia:.2f}</td>
+        <td style="text-align: center"><font color={cor_ticket_dia}>◙</font></td>
+    </tr>
+    </table>
+    <br>
+    <table>
+    <tr>
+        <th>Indicador</th>
+        <th style="text-align: center">Valor Ano</th>
+        <th style="text-align: center">Meta Ano</th>
+        <th style="text-align: center">Cenário Ano</th>
+    </tr>
+    <tr>
+        <td>Faturamento</td>
+        <td style="text-align: center">R${faturamento_ano:.2f}</td>
+        <td style="text-align: center">R${meta_faturamento_ano:.2f}</td>
+        <td style="text-align: center"><font color={cor_fat_ano}>◙</font></td>
+    </tr>
+    <tr>
+        <td>Diversidade de Produtos</td>
+        <td style="text-align: center">{qtde_produtos_ano}</td>
+        <td style="text-align: center">{meta_qtde_produtos_ano}</td>
+        <td style="text-align: center"><font color={cor_qtde_ano}>◙</font></td>
+    </tr>
+    <tr>
+        <td>Ticket Médio</td>
+        <td style="text-align: center">R${ticket_medio_ano:.2f}</td>
+        <td style="text-align: center">R${meta_ticket_medio_ano:.2f}</td>
+        <td style="text-align: center"><font color={cor_ticket_ano}>◙</font></td>
+    </tr>
+    </table>
+
+    <p>Segue aem anexo a planilha com todos os dados para mais detalhes.</p>
+
+    <p>Qualquer dúvida, estou à disposição.</p>
+    <p>Ayy., Antonny</p>
+    """
+
+    attachment = pathlib.Path.cwd() / caminho_backup / loja / f'{dia_indicador.month}_{dia_indicador.day}_{loja}.xlsx'
+    email.Attachments.Add(str(attachment))
+    email.Send()
+    print(f'E-mail da Loja {loja} enviado!')
+
+# %% [markdown]
+# ### Passo 5 - Enviar por e-mail para o gerente
+
+# %% [markdown]
+# ### Passo 6 - Automatizar todas as lojas
+
+# %% [markdown]
+# ### Passo 7 - Criar ranking para diretoria
+
+# %%
+faturamento_lojas = vendas.groupby('Loja')[['Loja', 'Valor Final']].sum(numeric_only=True)
+faturamento_lojas_ano = faturamento_lojas.sort_values(by='Valor Final', ascending=False)
+
+# Salvar dentro da pasta
+nome_arquivo = f'{dia_indicador.month}_{dia_indicador.day}_Ranking Anual.xlsx'
+faturamento_lojas_ano.to_excel(r'Backup Arquivos Lojas\{}'.format(nome_arquivo))
+
+vendas_dia = vendas.loc[vendas['Data']==dia_indicador, :]
+faturamento_lojas_dia = vendas_dia.groupby('Loja')[['Loja', 'Valor Final']].sum(numeric_only=True)
+faturamento_lojas_dia = faturamento_lojas_dia.sort_values(by='Valor Final', ascending=False)
+
+nome_arquivo = f'{dia_indicador.month}_{dia_indicador.day}_Ranking Dia.xlsx'
+faturamento_lojas_dia.to_excel(r'Backup Arquivos Lojas\{}'.format(nome_arquivo))
+
+# %% [markdown]
+# ### Passo 8 - Enviar e-mail para diretoria
+
+# %%
+# Enviar E-mail
+outlook = win32.Dispatch('outlook.application')
+
+email = outlook.CreateItem(0)
+
+# Processo de envio de email
+email.To = emails.loc[emails['Loja']=='Diretoria', 'E-mail'].values[0]
+email.Cc = "programacao.oficial2@gmail.com"
+email.Subject = f"Ranking Dia {dia_indicador.day}/{dia_indicador.month}"
+email.Body = f"""
+Prezados, bom dia
+
+Melhor Loja do Dia em faturamento: Loja {faturamento_lojas_dia.index[0]} com faturamento R${faturamento_lojas_dia.iloc[0, 0]:.2f}
+Pior Loja do Dia em faturamento: Loja {faturamento_lojas_dia.index[-1]} com faturamento R${faturamento_lojas_dia.iloc[-1, 0]:.2f}
+
+Melhor Loja do Ano em faturamento: Loja {faturamento_lojas_ano.index[0]} com faturamento R${faturamento_lojas_ano.iloc[0, 0]:.2f}
+Pior Loja do Ano em faturamento: Loja {faturamento_lojas_ano.index[-1]} com faturamento R${faturamento_lojas_ano.iloc[-1, 0]:.2f}
+
+
+Segue em anexo os rankings do ano e do dia de todas as lojas
+
+Qualquer dúvida, estou à disposição.
+
+Att.,
+Antonny
+"""
+
+attachment = pathlib.Path.cwd() / caminho_backup / f'{dia_indicador.month}_{dia_indicador.day}_Ranking Anual.xlsx'
+email.Attachments.Add(str(attachment))
+attachment = pathlib.Path.cwd() / caminho_backup / f'{dia_indicador.month}_{dia_indicador.day}_Ranking Dia.xlsx'
+email.Attachments.Add(str(attachment))
+
+email.Send()
+print(f'E-mail da Diretoria enviado!')
+
+
